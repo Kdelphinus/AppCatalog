0cb381efbf9fa78998be949a30968d06dc5cb996
Kdelphinus <kmj951015@gmail.com>
2025-08-28 13:31:44 +0900

    init project

---
+++ src/main/java/cmp/appcatalog/AppcatalogApplication.java
@@ -0,0 +1,13 @@
+package cmp.appcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppcatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppcatalogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cmp/appcatalog/AppcatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package cmp.appcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppcatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
