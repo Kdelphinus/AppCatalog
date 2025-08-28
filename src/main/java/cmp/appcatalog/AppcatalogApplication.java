2979862bd4430fad59d66ae4bb63fdc1ade6eddb
Ko Myoung Jun <kmj951015@gmail.com>
2025-08-29 00:08:07 +0900

    [style]: formatter 적용
    Signed-off-by: Ko Myoung Jun <kmj951015@gmail.com>

--- src/main/java/cmp/appcatalog/AppcatalogApplication.java
+++ src/main/java/cmp/appcatalog/AppcatalogApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AppcatalogApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AppcatalogApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(AppcatalogApplication.class, args);
+  }
 }
