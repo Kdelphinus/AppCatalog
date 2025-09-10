4013fcab611c3228a69a182320b14af2eca45a3a
ko myoung jun <kmj951015@gmail.com>
2025-09-10 10:30:26 +0900

    [refactor] Import 정리

--- src/test/java/com/appcatalog/target/TargetServiceTest.java
+++ src/test/java/com/appcatalog/target/TargetServiceTest.java
@@ -14,7 +14,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.List;
 import java.util.Optional;
 
-import static org.assertj.core.api.Assertions.as;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
