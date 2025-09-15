3f0e868c5d6019be0e93920cf4f8627e3e324c62
ko myoung jun <kmj951015@gmail.com>
2025-09-15 11:37:45 +0900

    [fix] 중복된 이름으로 Target 생성 요청 시, 예외 처리 로직 추가

--- src/main/java/com/appcatalog/target/domain/TargetEnvironmentRepository.java
+++ src/main/java/com/appcatalog/target/domain/TargetEnvironmentRepository.java
@@ -1,7 +1,11 @@
 package com.appcatalog.target.domain;
 
+import lombok.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TargetEnvironmentRepository extends JpaRepository<TargetEnvironment, Long> {}
+public interface TargetEnvironmentRepository extends JpaRepository<TargetEnvironment, Long> {
+
+  boolean existsByName(String name);
+}
